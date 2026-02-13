e443e534de5d3b6a458b3f6dfb3db3d93a499220
Eden Calderon <goncalderon27@gmail.com>
2026-02-13 15:32:47 +0800

    Adding Chatbot Agent

--- app/layout.js
+++ app/layout.js
@@ -9,18 +9,18 @@ const inter = Inter({
 
 export const metadata = {
   title: "CNT CloudSpace",
-  description: "Your Digital Workspace for faster, smarter, and safer collaboration.",
+  description:
+    "Your Digital Workspace for faster, smarter, and safer collaboration.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.variable} font-sans antialiased pt-20`}
-      >
+      <body className={`${inter.variable} font-sans antialiased pt-20`}>
         <Header />
         {children}
       </body>
+      <script src="https://cdn.jotfor.ms/agent/embedjs/019c55dbd73d73689bcf8cfa8216ac931889/embed.js"></script>
     </html>
   );
 }
